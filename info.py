ba45f6bb45852e77f9ea3c325641ece32ef8f988
subho997 <subhojitpatowari33@gmail.com>
2025-08-03 12:22:55 +0530

    Update info.py

--- info.py
+++ info.py
@@ -24,10 +24,10 @@ LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002891364988'))
 MONGODB_URI = environ.get("MONGODB_URI", "")
 
 # Stream Url Means Your Deploy Server App Url, Here You Media Will Be Stream And Ads Will Be Shown.
-STREAM_URL = environ.get("STREAM_URL", "")
+STREAM_URL = environ.get("STREAM_URL", "https://watchonlinespk99.blogspot.com/2025/07/spk.html")
 
 # This Link Used As Permanent Link That If Your Deploy App Deleted Then You Change Stream Url, So This Link Will Redirect To Stream Url.
-LINK_URL = environ.get("LINK_URL", "https://watchonlinespk99.blogspot.com/2025/07/spk.html")
+LINK_URL = environ.get("LINK_URL", "")
 
 # Others, Not Usefull
 PORT = environ.get("PORT", "8080")
